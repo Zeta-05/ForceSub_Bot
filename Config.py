ebf01192ebdef77ff2a98d1025d17ab7c56833ec
Zeta-05 <vjimmy276@gmail.com>
2024-12-02 13:30:06 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,11 +8,11 @@ class Config():
   # Heroku postgres DB URL
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
   # get it from my.telegram.org
-  APP_ID = os.environ.get("APP_ID", 123456)
-  API_HASH = os.environ.get("API_HASH", "")
+  APP_ID = os.environ.get("APP_ID", 23568641)
+  API_HASH = os.environ.get("API_HASH", "a39098e8752a45c2d6d1889941547bbc")
   # Sudo users( goto @JVToolsBot and send /id to get your id)
-  SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "1204927413 1405957830").split()))
-  SUDO_USERS.append(1204927413)
+  SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "7318101682").split()))
+  SUDO_USERS.append(7318101682)
   SUDO_USERS = list(set(SUDO_USERS))
 
 class Messages():
@@ -25,7 +25,7 @@ class Messages():
         
         "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username or channel ID} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n/source_code - To get bot source code😍\n\nNote: /FSub is an alias of /ForceSubscribe__",
         
-       "**Devloped By @UniversalBotsUpdate**"
+       "**Devloped By @Kayto_Official**"
       ]
       SC_MSG = "**Hey [{}](tg://user?id={})**\n click on below👇 button to get my source code, for more help ask in my support group👇👇 "
 
